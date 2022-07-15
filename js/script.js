2162b6ab948229d5dc5cda84b5bc98f7197a18ba
kaiqsou <kaiqsouza2004@hotmail.com>
2022-07-15 11:17:16 -0300

    CSS e JS

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function mudar(cor)
+{
+    document.body.style.background = cor;
+}
+
+function borda(corBorda)
+{
+    b1.style.borderColor = corBorda;
+    b2.style.borderColor = corBorda;
+    b3.style.borderColor = corBorda;
+    b4.style.borderColor = corBorda;
+    b5.style.borderColor = corBorda;
+}
